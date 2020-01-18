77fceb131d7d3330ae0a28681455719bc1eb0150
Kurt Van Etten <kurt@vanetten.us>
2020-01-18 16:09:30 -0500

    Add default route handler

--- index.js
+++ index.js
@@ -5,16 +5,34 @@ const helmet = require('helmet');
 
 const port = process.env.PORT || 5000;
 
+const defaultPage = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello Server</title>
+    </head>
+    <body>
+        <h1>Hello, Server!</h1>
+        <h3>Available endpoints:</h3>
+        <ul>
+            <li>/api/login</li>
+            <li>/api/users</li>
+        </ul>
+        <hr>
+        <p><span style="font-style:italic">
+            Server listening on port ${port}
+        </span></p>
+    </body>
+    </html>
+`;
+
 const server = express();
-server.use(helmet);
+server.use(helmet());
 server.use(express.json());
 
 // default 'proof of life' endpoint
 server.get('/', (req, res, next) => {
-  const defaultPage = `
-        <h1>Hello, World!</h1>
-        <p>Server listening on port ${port}</p>
-    `;
   res.send(defaultPage);
 });
 
